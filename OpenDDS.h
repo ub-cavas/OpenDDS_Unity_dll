e87a26aa216eb67f881981592c6de742882bc41d
Roman Dmowski <roman.dmowski@outlook.com>
2018-05-22 13:17:30 -0400

    removed unused functions v2x

--- OpenDDS.h
+++ OpenDDS.h
@@ -25,15 +25,15 @@ void garbageCollectionMap();
 
 
 
-long generateV2xUniqueTimestamp(long v2x_timestamp);
-
-void addV2xToMap(long extended_timestamp, Mri::V2XMessage v2x);
-
-void transmitV2X(Mri::V2XMessage v2x, long destination_veh_id);
+//long generateV2xUniqueTimestamp(long v2x_timestamp);
+//
+//void addV2xToMap(long extended_timestamp, Mri::V2XMessage v2x);
+//
+//void transmitV2X(Mri::V2XMessage v2x, long destination_veh_id);
 
 void publishVehDataMessage(Mri::VehData car);
 
-void publishV2xMessage(Mri::V2XMessage v2x);
+//void publishV2xMessage(Mri::V2XMessage v2x);
 
 void ProcessDoNotPassWarningMessage(Mri::Aux2Strings dnpwAux);
 
--- OpenDDST.cpp
+++ OpenDDST.cpp
@@ -522,87 +522,87 @@ void garbageCollectionMap()
 
 
 
+//
+//long generateV2xUniqueTimestamp(long v2x_timestamp) {
+//
+//	long timestamp_now = GetTimestamp();
+//	if (timestamp_now > timestamp_previous)
+//	{
+//		sample_counter_per_timestamp = 0;
+//		timestamp_previous = timestamp_now;
+//	}
+//	else
+//	{
+//		sample_counter_per_timestamp++;
+//	}
+//	return ((v2x_timestamp * 1000) + sample_counter_per_timestamp);
+//}
 
-long generateV2xUniqueTimestamp(long v2x_timestamp) {
-
-	long timestamp_now = GetTimestamp();
-	if (timestamp_now > timestamp_previous)
-	{
-		sample_counter_per_timestamp = 0;
-		timestamp_previous = timestamp_now;
-	}
-	else
-	{
-		sample_counter_per_timestamp++;
-	}
-	return ((v2x_timestamp * 1000) + sample_counter_per_timestamp);
-}
-
-void addV2xToMap(long extended_timestamp, Mri::V2XMessage v2x) {
-
-	std::map<long, Mri::V2XMessage>::iterator it_v2x;
-
-	it_v2x = v2xs_map.find(extended_timestamp);
-
-	if (it_v2x != v2xs_map.end())
-	{
-		std::cerr << "Error - duplicated elements in v2xs_map." << std::endl;
-	}
-	else
-	{
-		v2xs_map.emplace(extended_timestamp, v2x);
-	}
-}
-
-
-void transmitV2X(Mri::V2XMessage v2x, long destination_veh_id) {
-
-	const double V2X_RANGE = 300.0; //150 meters
-
-	try
-	{
-		if (v2x.sender_id != destination_veh_id)
-		{
-			Mri::VehData veh_sender = vehs_map[v2x.sender_id];
-			Mri::VehData veh_receiver = vehs_map[destination_veh_id];
+//void addV2xToMap(long extended_timestamp, Mri::V2XMessage v2x) {
+//
+//	std::map<long, Mri::V2XMessage>::iterator it_v2x;
+//
+//	it_v2x = v2xs_map.find(extended_timestamp);
+//
+//	if (it_v2x != v2xs_map.end())
+//	{
+//		std::cerr << "Error - duplicated elements in v2xs_map." << std::endl;
+//	}
+//	else
+//	{
+//		v2xs_map.emplace(extended_timestamp, v2x);
+//	}
+//}
 
 
-
-			double distance = sqrt(pow((veh_receiver.position_x - veh_sender.position_x), 2.0) + pow((veh_receiver.position_y - veh_sender.position_y), 2.0));
-
-			if (distance < V2X_RANGE)
-			{
-				//transmit
-				long  delay = rand() % 50 + 15;	// delay 15 - 64 ms
-												//update data in v2x message
-
-				v2x.recipient_id = destination_veh_id;
-				v2x.recipient_timestamp = v2x.sender_timestamp + (delay / 10);
-
-				// resend v2x
-				
-
-				publishV2xMessage(v2x);
-
-			}
-			else
-			{
-				v2x.recipient_id = destination_veh_id;
-				std::cout << endl << "-- NO TRANSMITION from veh_id:" << v2x.sender_id << " to veh_id:" << v2x.recipient_id << " timestamps: "
-					<< v2x.sender_timestamp << " distance=" << distance << std::endl;
-			}
-		}
-
-
-
-	}
-	catch (const std::exception&)
-	{
-		//do nothing
-		//transmit nothing
-	}
-
-}
+//void transmitV2X(Mri::V2XMessage v2x, long destination_veh_id) {
+//
+//	const double V2X_RANGE = 300.0; //150 meters
+//
+//	try
+//	{
+//		if (v2x.sender_id != destination_veh_id)
+//		{
+//			Mri::VehData veh_sender = vehs_map[v2x.sender_id];
+//			Mri::VehData veh_receiver = vehs_map[destination_veh_id];
+//
+//
+//
+//			double distance = sqrt(pow((veh_receiver.position_x - veh_sender.position_x), 2.0) + pow((veh_receiver.position_y - veh_sender.position_y), 2.0));
+//
+//			if (distance < V2X_RANGE)
+//			{
+//				//transmit
+//				long  delay = rand() % 50 + 15;	// delay 15 - 64 ms
+//												//update data in v2x message
+//
+//				v2x.recipient_id = destination_veh_id;
+//				v2x.recipient_timestamp = v2x.sender_timestamp + (delay / 10);
+//
+//				// resend v2x
+//				
+//
+//				publishV2xMessage(v2x);
+//
+//			}
+//			else
+//			{
+//				v2x.recipient_id = destination_veh_id;
+//				std::cout << endl << "-- NO TRANSMITION from veh_id:" << v2x.sender_id << " to veh_id:" << v2x.recipient_id << " timestamps: "
+//					<< v2x.sender_timestamp << " distance=" << distance << std::endl;
+//			}
+//		}
+//
+//
+//
+//	}
+//	catch (const std::exception&)
+//	{
+//		//do nothing
+//		//transmit nothing
+//	}
+//
+//}
 
 void publishVehDataMessage(Mri::VehData car) {
 	int success = writer_global_vehdata->write(car, DDS::HANDLE_NIL);
