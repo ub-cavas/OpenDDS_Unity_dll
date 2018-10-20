361899e0519f1c684c105cab4bc0fec387f917ae
Roman Dmowski <roman.dmowski@outlook.com>
2018-10-20 10:01:37 -0400

    rebuild all project, in new folder

--- MriC.h
+++ MriC.h
@@ -28,8 +28,8 @@
 // TAO_IDL - Generated from
 // be\be_codegen.cpp:152
 
-#ifndef _TAO_IDL_MRIC_GHSYHV_H_
-#define _TAO_IDL_MRIC_GHSYHV_H_
+#ifndef _TAO_IDL_MRIC_MP8JHP_H_
+#define _TAO_IDL_MRIC_MP8JHP_H_
 
 #include /**/ "ace/pre.h"
 
--- MriS.cpp
+++ MriS.cpp
@@ -29,8 +29,8 @@
 // TAO_IDL - Generated from
 // be\be_codegen.cpp:652
 
-#ifndef _TAO_IDL_MRIS_IDECSJ_CPP_
-#define _TAO_IDL_MRIS_IDECSJ_CPP_
+#ifndef _TAO_IDL_MRIS_P3JXUG_CPP_
+#define _TAO_IDL_MRIS_P3JXUG_CPP_
 
 
 #include "MriS.h"
